8cf30b385c92e1936dcb8f249bc98e9ef09d4cf6
Ян Хи Чан <yan.k.ch@sberbank.ru>
2019-07-28 18:22:05 +0300

    22

--- hw_streams/splitFile.js
+++ hw_streams/splitFile.js
@@ -1,4 +1,5 @@
-const fs = require('fs');
+const fs = require("fs");
+const chalk = require("chalk");
 // const pathToInitFile = require('./hw_streams');
 
 // fs.readFile(pathToInitFile, (err, data) => {
@@ -12,14 +13,35 @@ const fs = require('fs');
 // 	console.log(data);
 // }
 
-const file1 = 'file1.js';
-const file2 = 'file2.js';
-const file3 = 'file3.js';
+const file1 = "file1.js";
+const file2 = "file2.js";
+const file3 = "file3.js";
 
-const writeStream1 = fs.createWriteStream(file1);
-const readStream = fs.createReadStream('test.js');
-readStream.pipe(writeStream1)
+// const writeStream1 = fs.createWriteStream(file1);
+// writeStream1.write('heelo')
+// const readStream = fs.createReadStream('test.js');
+// readStream.pipe(writeStream1)
 
-// writeStream1.write('234');
-// const writeStream1 = fs.createWriteStream()
-// const writeStream1 = fs.createWriteStream()
+function main(params) {
+	const stream1 = fs.createReadStream("./file1.js", { highWaterMark: 1 });
+	const stream2 = fs.createReadStream("./file2.js", { highWaterMark: 1 });
+
+	const streamsArr = [stream1, stream2];
+
+	streamsArr.forEach(async stream => {
+		for await (let chunk of stream) {
+			cache += chunk;
+			console.log(cache);
+		}
+	});
+
+}
+
+// async function* somefn(chunksAsync) {
+// 	for await (let chunk of chunksAsync) {
+// 		console.log(chunk.toString());
+// 		yield chunk;
+// 	}
+// }
+
+const get = main();
